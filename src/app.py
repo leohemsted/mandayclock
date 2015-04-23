b9bcfef32f175a3983701876844f712fba591e05
Leo Hemsted <leo.hemsted@artsalliancemedia.com>
2015-04-23 10:39:26 +0100

    simple list

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = flask.Flask('mandayclock')
 
 @app.route('/')
 def i_dont_even_know():
-    return render_template('index.html', stuff=[1, 'poo', False])
+    return flask.render_template('index.html', stuff=[1, 'poo', False])
 
 if __name__ == "__main__":
     app.run(debug=True)
