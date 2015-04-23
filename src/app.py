45942e31e7732bb5c12c4ee9b294a290d27b500a
Leo Hemsted <leo.hemsted@artsalliancemedia.com>
2015-04-23 10:38:22 +0100

    debug mode is god

--- src/app.py
+++ src/app.py
@@ -3,14 +3,9 @@ import flask
 
 app = flask.Flask('mandayclock')
 
-
 @app.route('/')
 def i_dont_even_know():
-    return (
-"""
-HELLO
-"""
-)
+    return render_template('index.html', stuff=[1, 'poo', False])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
